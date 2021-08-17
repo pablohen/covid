b20a90253a66b5533d18b31cdb27443ad7ada5a4
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-17 11:01:17 -0300

    .

---
+++ components/CustomLoader.tsx
@@ -0,0 +1,18 @@
+import Loader from 'react-loader-spinner';
+
+interface Props {
+  text: string;
+}
+
+const CustomLoader = (props: Props) => {
+  const { text } = props;
+
+  return (
+    <div className="flex flex-col items-center space-y-2">
+      <span className="font-bold text-lg text-purple-600">{text}</span>
+      <Loader type="Circles" color="gray" />
+    </div>
+  );
+};
+
+export default CustomLoader;
