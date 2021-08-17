7ff2ee54f0fd3698959e02bdaefd110684109a86
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-17 14:27:50 -0300

    add Poppins webfont

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,16 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
