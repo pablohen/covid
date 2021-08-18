dbe36092cb71dace3e21f87dcfc970eb91fec9a2
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-18 09:16:16 -0300

    add Footer

--- pages/[city].tsx
+++ pages/[city].tsx
@@ -6,6 +6,7 @@ import { Bar } from 'react-chartjs-2';
 import CountUp from 'react-countup';
 import { NextSeo } from 'next-seo';
 import CustomLoader from '../components/CustomLoader';
+import Footer from '../components/Footer';
 
 interface Report {
   city: string;
@@ -68,59 +69,62 @@ const CityCasesPage = (props: Props) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center bg-purple-50 min-h-screen">
-      {router.isFallback ? (
-        <CustomLoader text="Carregando..." />
-      ) : (
-        <div className="flex flex-col p-4 space-x-0 space-y-4 w-full lg:items-center">
-          <NextSeo title={`${reports[0].city}/${reports[0].state}`} />
-          <div className="flex flex-col justify-center items-center text-center md:flex-row md:justify-between md:text-left w-full lg:w-1/2 h-full border border-purple-400 rounded bg-white p-4 pt-8 md:pt-8 shadow-md relative">
-            <div className="pb-4 space-y-1">
-              <p className="font-bold text-2xl text-purple-500">
-                {`${reports[0].city}/${reports[0].state}`}
-              </p>
-              <p className="font-bold text-xs text-purple-500">
-                {reports[0].estimated_population} habitantes (estimativa)
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col flex-grow justify-center items-center bg-purple-50">
+        {router.isFallback ? (
+          <CustomLoader text="Carregando..." />
+        ) : (
+          <div className="flex flex-col p-4 space-x-0 space-y-4 w-full lg:items-center">
+            <NextSeo title={`${reports[0].city}/${reports[0].state}`} />
+            <div className="flex flex-col justify-center items-center text-center md:flex-row md:justify-between md:text-left w-full lg:w-1/2 h-full border border-purple-400 rounded bg-white p-4 pt-8 md:pt-8 shadow-md relative">
+              <div className="pb-4 space-y-1">
+                <p className="font-bold text-2xl text-purple-500">
+                  {`${reports[0].city}/${reports[0].state}`}
+                </p>
+                <p className="font-bold text-xs text-purple-500">
+                  {reports[0].estimated_population} habitantes (estimativa)
+                </p>
+              </div>
+
+              <div>
+                <p className="text-sm text-purple-500">
+                  <CountUp
+                    end={reports[0].confirmed}
+                    duration={1}
+                    className="font-bold"
+                  />{' '}
+                  casos confirmados
+                </p>
+                <p className="text-sm text-purple-500">
+                  <CountUp
+                    end={reports[0].deaths}
+                    duration={1}
+                    className="font-bold"
+                  />{' '}
+                  óbitos
+                </p>
+                <p className="text-sm text-purple-500">
+                  <span className="font-bold">
+                    {Number(reports[0].death_rate * 100).toPrecision(2)}%
+                  </span>{' '}
+                  de letalidade
+                </p>
+              </div>
+
+              <p className="absolute text-white text-xs font-bold bg-purple-400 p-1 rounded-tr rounded-bl top-0 right-0">
+                {`Atualizado em: ${new Date(
+                  reports[0].date
+                ).toLocaleDateString()}`}
               </p>
             </div>
 
-            <div>
-              <p className="text-sm text-purple-500">
-                <CountUp
-                  end={reports[0].confirmed}
-                  duration={1}
-                  className="font-bold"
-                />{' '}
-                casos confirmados
-              </p>
-              <p className="text-sm text-purple-500">
-                <CountUp
-                  end={reports[0].deaths}
-                  duration={1}
-                  className="font-bold"
-                />{' '}
-                óbitos
-              </p>
-              <p className="text-sm text-purple-500">
-                <span className="font-bold">
-                  {Number(reports[0].death_rate * 100).toPrecision(2)}%
-                </span>{' '}
-                de letalidade
-              </p>
+            <div className="w-full lg:w-10/12 bg-white rounded border border-purple-400 shadow-md p-4">
+              <Bar data={data} options={options} />
             </div>
-
-            <p className="absolute text-white text-xs font-bold bg-purple-400 p-1 rounded-tr rounded-bl top-0 right-0">
-              {`Atualizado em: ${new Date(
-                reports[0].date
-              ).toLocaleDateString()}`}
-            </p>
           </div>
-
-          <div className="w-full lg:w-10/12 bg-white rounded border border-purple-400 shadow-md p-4">
-            <Bar data={data} options={options} />
-          </div>
-        </div>
-      )}
+        )}
+      </div>
+      <Footer />
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Autocomplete from 'react-autocomplete';
 import router from 'next/router';
 import CustomLoader from '../components/CustomLoader';
 import { slugify } from 'transliteration';
+import Footer from '../components/Footer';
 
 interface City {
   id: string;
@@ -31,54 +32,57 @@ const Home = (props: Props) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-purple-50 space-y-4">
-      <h1 className="text-2xl font-bold text-purple-800">
-        Estatísticas Covid-19
-      </h1>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col flex-grow justify-center items-center  bg-purple-50 space-y-4">
+        <h1 className="text-2xl font-bold text-purple-800">
+          Estatísticas Covid-19
+        </h1>
 
-      <div className="w-full">
-        <Autocomplete
-          getItemValue={(city: City) => city.name}
-          items={filterList()}
-          renderItem={(city: City, isHighlighted: Boolean) => (
-            <div
-              key={city.id}
-              className={`px-2 ${
-                isHighlighted ? 'bg-purple-200' : 'bg-transparent'
-              }`}
-            >
-              {city.name}
-            </div>
-          )}
-          value={chosenCity}
-          onChange={(e: any) => setChosenCity(e.target.value)}
-          onSelect={(val: string, item: City) => {
-            setChosenCity(val);
-            router.push(`/${item.id}`);
-            toggleLoading();
-          }}
-          wrapperProps={{
-            style: { display: 'flex' },
-            className: 'flex justify-center',
-          }}
-          inputProps={{
-            placeholder: 'Escolha uma cidade do Brasil...',
-            className:
-              'flex border rounded shadow-sm px-4 py-2 w-10/12 lg:w-4/12 focus:shadow-lg focus:outline-none focus:ring-0 focus:border-purple-500',
-          }}
-          menuStyle={{
-            borderRadius: '3px',
-            boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
-            background: 'rgba(255, 255, 255, 0.9)',
-            padding: '2px 0',
-            position: 'fixed',
-            overflow: 'auto',
-            maxHeight: '10em',
-          }}
-        />
+        <div className="w-full">
+          <Autocomplete
+            getItemValue={(city: City) => city.name}
+            items={filterList()}
+            renderItem={(city: City, isHighlighted: Boolean) => (
+              <div
+                key={city.id}
+                className={`px-2 ${
+                  isHighlighted ? 'bg-purple-200' : 'bg-transparent'
+                }`}
+              >
+                {city.name}
+              </div>
+            )}
+            value={chosenCity}
+            onChange={(e: any) => setChosenCity(e.target.value)}
+            onSelect={(val: string, item: City) => {
+              setChosenCity(val);
+              router.push(`/${item.id}`);
+              toggleLoading();
+            }}
+            wrapperProps={{
+              style: { display: 'flex' },
+              className: 'flex justify-center',
+            }}
+            inputProps={{
+              placeholder: 'Escolha uma cidade do Brasil...',
+              className:
+                'flex border rounded shadow-sm px-4 py-2 w-10/12 lg:w-4/12 focus:shadow-lg focus:outline-none focus:ring-0 focus:border-purple-500',
+            }}
+            menuStyle={{
+              borderRadius: '3px',
+              boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+              background: 'rgba(255, 255, 255, 0.9)',
+              padding: '2px 0',
+              position: 'fixed',
+              overflow: 'auto',
+              maxHeight: '10em',
+            }}
+          />
+        </div>
+
+        {loading && <CustomLoader text="Aguarde..." />}
       </div>
-
-      {loading && <CustomLoader text="Aguarde..." />}
+      <Footer />
     </div>
   );
 };
