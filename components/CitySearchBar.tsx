68e9912e933d45fa11990d0e873e2c3417c1b5fa
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-19 10:31:10 -0300

    create component

---
+++ components/CitySearchBar.tsx
@@ -0,0 +1,70 @@
+import router from 'next/router';
+import { useState } from 'react';
+import Autocomplete from 'react-autocomplete';
+import slugify from 'slugify';
+import City from '../interfaces/City';
+import { Toaster, toast } from 'react-hot-toast';
+
+interface Props {
+  cities: City[];
+}
+
+const CitySearchBar = (props: Props) => {
+  const { cities } = props || {};
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const filterList = () => {
+    return cities.filter((city) =>
+      slugify(city.name, { lower: true }).includes(
+        slugify(selectedItem, { lower: true })
+      )
+    );
+  };
+
+  return (
+    <div className="w-full">
+      <Autocomplete
+        getItemValue={(item: City) => item.name}
+        items={filterList()}
+        renderItem={(item: City, isHighlighted: Boolean) => (
+          <div
+            key={item.id}
+            className={`px-2 ${
+              isHighlighted ? 'bg-purple-200' : 'bg-transparent'
+            }`}
+          >
+            {item.name}
+          </div>
+        )}
+        value={selectedItem}
+        onChange={(e: any) => setSelectedItem(e.target.value)}
+        onSelect={(val: string, item: City) => {
+          setSelectedItem(val);
+          router.push(`/${item.id}`);
+          toast.loading('Carregando...', { position: 'bottom-center' });
+        }}
+        wrapperProps={{
+          style: { display: 'flex' },
+          className: 'flex justify-center',
+        }}
+        inputProps={{
+          placeholder: 'Escolha uma cidade do Brasil...',
+          className:
+            'flex border rounded shadow-sm px-4 py-2 w-10/12 lg:w-4/12 focus:shadow-lg focus:outline-none focus:ring-0 focus:border-purple-500',
+        }}
+        menuStyle={{
+          borderRadius: '3px',
+          boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+          background: 'rgba(255, 255, 255, 0.9)',
+          padding: '2px 0',
+          position: 'fixed',
+          overflow: 'auto',
+          maxHeight: '10em',
+        }}
+      />
+      <Toaster />
+    </div>
+  );
+};
+
+export default CitySearchBar;
