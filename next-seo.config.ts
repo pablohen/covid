67dd38e322588680d123e6d9ce2ed5e49a85ae1c
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-24 21:03:08 -0300

    renamed and removed keywords

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,13 +2,10 @@ const site = `https://${process.env.VERCEL_URL}`;
 const siteName = 'Estatísticas Covid-19 Brasil';
 const description =
   'Veja as estatísticas da Covid-19 de cada município do Brasil';
-const keywords =
-  'Covid-19, estatísticas, Brasil, pablohen, next, react, js, tailwind, css';
 const nextSeo = {
   defaultTitle: siteName,
   titleTemplate: `%s | ${siteName}`,
   description,
-  keywords,
   openGraph: {
     type: 'website',
     locale: 'pt-BR',
