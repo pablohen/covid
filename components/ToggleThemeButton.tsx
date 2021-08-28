210a14b93bfc5518644b450dc5336645b8726e65
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-28 19:38:23 -0300

    fix system theme bug

--- components/ToggleThemeButton.tsx
+++ components/ToggleThemeButton.tsx
@@ -3,11 +3,11 @@ import { useTheme } from 'next-themes';
 import React, { useEffect, useState } from 'react';
 
 const ToggleThemeButton = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   const [loading, setLoading] = useState(true);
 
   const toggleTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
   };
 
   useEffect(() => {
@@ -20,12 +20,12 @@ const ToggleThemeButton = () => {
         <div className="flex flex-wrap justify-center items-center">
           <button onClick={toggleTheme} className="p-2">
             <p className="flex items-center p-1 px-2 border border-white dark:border-gray-500 rounded-full text-white transform transition-all duration-150 ease-in-out hover:bg-purple-400 dark:hover:bg-gray-600">
-              {theme === 'dark' ? (
+              {resolvedTheme === 'dark' ? (
                 <MoonIcon className="h-6 mr-2" />
               ) : (
                 <SunIcon className="h-6 mr-2" />
               )}
-              {theme}
+              {resolvedTheme}
             </p>
           </button>
         </div>
