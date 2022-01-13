32b23ec16ce8bdfc497e2d9bd2d76f7f85458256
Pablo Henrique <pablo.henrique@cubos.io>
2022-01-13 08:49:36 -0300

    fix: :bug: disable Loader

--- src/components/CustomLoader.tsx
+++ src/components/CustomLoader.tsx
@@ -1,4 +1,5 @@
-import Loader from 'react-loader-spinner';
+// import Loader from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 interface Props {
   text: string;
@@ -8,7 +9,7 @@ const CustomLoader = ({ text }: Props) => {
   return (
     <div className="flex flex-col items-center space-y-2">
       <span className="font-bold text-lg text-white">{text}</span>
-      <Loader type="Circles" color="#ffffffaa" />
+      {/* <Loader type="Circles" color="#ffffffaa" /> */}
     </div>
   );
 };
